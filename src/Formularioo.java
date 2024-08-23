03c6404e0c743e39a9ceaee0babc9e41e8071438
Igor Castelo <freitascasteloigor@gmail.com>
2024-08-23 18:59:54 -0300

    Exercicio-intermediario2

--- src/Formularioo.java
+++ src/Formularioo.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Formularioo {
+public class Formularioo {
+
+    public static boolean verificarNome(String nome) {
+        return nome.matches("[A-Za-z ]+");
+    }
+
+    public static boolean verificarSobrenome(String sobrenome) {
+        return sobrenome.matches("[A-Za-z ]+");
+    }
+
+    public static boolean verificarCPF(String cpf) {
+        return cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}");
+    }
+
+    public static boolean verificarEmail(String email) {
+        return email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+    }
+
 }
